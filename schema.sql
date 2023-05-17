e1e85c3a3f6a80cdfd925b1b30b9ab35b9d5c394
everinurmind <nurbol.sultanov@gmail.com>
2023-05-16 21:32:33 -0600

    Add prim key

--- schema.sql
+++ schema.sql
@@ -1,10 +1,10 @@
 /* Database schema to keep the structure of entire database. */
 
 CREATE TABLE animals (
-  id INT,
-  name VARCHAR(255),
-  date_of_birth DATE,
-  escape_attempts INT,
-  neutered BOOLEAN,
-  weight_kg DECIMAL(5, 2)
+  id INT NOT NULL,
+  name VARCHAR(255) NOT NULL,
+  date_of_birth DATE NOT NULL,
+  escape_attempts INT NOT NULL,
+  neutered BOOLEAN NOT NULL,
+  weight_kg DECIMAL(5, 2) NOT NULL
 );
